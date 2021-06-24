4a88a75496fdbfcc022730b8664ff6b9fc0c2b5d
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-24 16:58:51 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,7 +4,15 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+  this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
