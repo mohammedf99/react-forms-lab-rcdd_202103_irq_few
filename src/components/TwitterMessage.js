f06cd59d844bbede464a61968a2c0573200ff048
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-24 16:59:14 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -20,7 +20,8 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={this.handleChange} value={this.state.message}/>
+        {charNumber}
       </div>
     );
   }
